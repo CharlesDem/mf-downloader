477229a78973343d0899cb79d42fdc37bbd43097
CharlesDem <24276208+CharlesDem@users.noreply.github.com>
2026-01-14 22:50:06 +0100

    feat: hardening dl with retries

--- file_helper.py
+++ file_helper.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import os
+import time
 from typing import BinaryIO, cast
 from dotenv import load_dotenv
+from minio import S3Error
 import requests
 import structlog
 from minio_config import minio_client, ensure_bucket, bufr_bucket
@@ -29,28 +31,43 @@ def dl_file(url: str):
             f.write(chunk)
         log.info(f"File gz downloaded from mf api: {dest_file}")
         
-def dl_file_to_s3(url: str):
+
+MAX_RETRIES = 5
+RETRY_DELAY = 20
+
+
+def dl_file_to_s3(url: str) -> None:
 
     ensure_bucket(minio_client, bufr_bucket)
 
-    r = requests.get(url, stream=True, timeout=25, headers=headers)
-    r.raise_for_status()
+    for attempt in range(1, MAX_RETRIES + 1):
+        try:
 
-    data = cast(BinaryIO, r.raw)
-    dest_name = dest_path("")
+            r = requests.get(url, stream=True, timeout=20, headers=headers)
+            r.raise_for_status()
 
-    minio_client.put_object(
-        bucket_name=bufr_bucket,
-        object_name=dest_name,
-        data=data,                 
-        length=-1,                  
-        part_size=10 * 1024 * 1024
-    )
+            data = cast(BinaryIO, r.raw)
+            dest_name = dest_path("")
 
-    log.info(
-        f"File gz dowloaded from api to minio: {dest_name}"
-    )
+            minio_client.put_object(
+                bucket_name=bufr_bucket,
+                object_name=dest_name,
+                data=data,
+                length=-1,
+                part_size=10 * 1024 * 1024,
+            )
 
+            log.info("file_uploaded_to_minio", object_name=dest_name)
+            return
+
+        except (requests.RequestException, S3Error) as e:
+            log.error("upload_failed", attempt=attempt, error=str(e))
+
+            if attempt == MAX_RETRIES:
+                log.error("giving_up", attempts=MAX_RETRIES)
+                return
+
+            time.sleep(RETRY_DELAY)
 
 def dest_path(target_dir: str) -> str:
     return f"{target_dir}/radar-moz-metr-{datetime.now().strftime("%Y-%m-%d--%H-%M-%S")}.gz"
